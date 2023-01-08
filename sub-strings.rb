b7dce4cc139c6403d030d65e37c3005b9191936f
kyungminkim1 <kyungminkimnz@gmail.com>
2023-01-08 16:25:55 +1300

    Create substrings pseudocode

---
+++ sub-strings.rb
@@ -0,0 +1,11 @@
+# function substring accepts two arguments: a word (string)
+# and a dictionary (array)
+
+# create empty hash 'counts' with argument 0
+# for each substring element in dictionary:
+    # set word to lowercase
+    # set substring to lowercase
+    # check if word includes that substring
+        # add substring as key to counts
+
+    # return counts
